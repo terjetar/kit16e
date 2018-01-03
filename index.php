e51287683648475f5fb91e05c342679998028185
Tehnikumi Opilane <opilane@tptlive.ee>
2018-01-03 13:16:47 +0200

    tere tere

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$greeting = "Hello";
+$nr = 1;
+$nr = 10.0121;
+$bool = False;
+$massiiv = array('Nimi', 1974, 10.81, 'perenimi');
+
+echo "$greeting World,<br>";
+echo "$greeting world!";
+echo $massiiv[0];
+
+
+ ?>
